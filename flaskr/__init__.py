508231e041795300c54dcb6e2d88675ac5b67ae3
Angel Llosa <anllogui@gmail.com>
2018-08-22 12:03:27 +0200

    First API with prediction.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,29 @@
 import os
+from sklearn.externals import joblib 
 
-from flask import Flask
+from flask import (
+    Flask, Blueprint, request, session, url_for, jsonify
+)
+
+from logging.config import dictConfig
+import logging
+from logging.handlers import RotatingFileHandler
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 
 def create_app(test_config=None):
@@ -10,6 +33,10 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+
+    handler = RotatingFileHandler('logs/foo.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -27,6 +54,28 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/hello')
     def hello():
+        app.logger.info("Hello!")
+
         return 'Hello, World!'
 
+    # prediction service
+    @app.route("/predict", methods=['POST'])
+    def predict():
+        if request.method == 'POST':
+            try:
+                data = request.get_json()
+                app.logger.info("data: %s", data["yearsOfExperience"])
+                years_of_experience = float(data["yearsOfExperience"])
+                lin_reg = joblib.load("models/linear_regression_model.pkl")
+            except ValueError:
+                return jsonify("Please enter a number.")
+
+            return jsonify(lin_reg.predict(years_of_experience).tolist())
+            
+    from . import api
+    app.register_blueprint(api.bp)
+
+    """
+    curl -i -H "Content-Type: application/json" -X POST -d '{"yearsOfExperience":8}' http://localhost:5000/predict
+    """
     return app
---
+++ initialize.sh
@@ -0,0 +1,7 @@
+WORKSPACE=.
+PYENV_HOME=$WORKSPACE/.pythonCI/
+export LC_ALL=es_ES.utf-8
+export LANG=es_ES.utf-8
+export FLASK_APP=$WORKSPACE/flaskr
+export FLASK_ENV=development
+export PATH=$PATH:/Users/anllogui/anaconda3/bin
