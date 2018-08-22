437e741678b6a785cf1da05e3b4857a44ec78e3b
Angel Llosa <anllogui@gmail.com>
2018-08-22 14:03:29 +0200

    Added retrain.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,7 @@
 import os
 from sklearn.externals import joblib 
+import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 from flask import (
     Flask, Blueprint, request, session, url_for, jsonify
@@ -78,4 +80,53 @@ def create_app(test_config=None):
     """
     curl -i -H "Content-Type: application/json" -X POST -d '{"yearsOfExperience":8}' http://localhost:5000/predict
     """
+
+    @app.route("/retrain", methods=['POST'])
+    def retrain():
+        if request.method == 'POST':
+            data = request.get_json()
+
+            try:
+                training_set = joblib.load("./training_data.pkl")
+                training_labels = joblib.load("./training_labels.pkl")
+
+                df = pd.read_json(data)
+
+                df_training_set = df.drop(["Salary"], axis=1)
+                df_training_labels = df["Salary"]
+
+                df_training_set = pd.concat([training_set, df_training_set])
+                df_training_labels = pd.concat([training_labels, df_training_labels])
+
+                new_lin_reg = LinearRegression()
+                new_lin_reg.fit(df_training_set, df_training_labels)
+
+                os.remove("./linear_regression_model.pkl")
+                os.remove("./training_data.pkl")
+                os.remove("./training_labels.pkl")
+
+                joblib.dump(new_lin_reg, "linear_regression_model.pkl")
+                joblib.dump(df_training_set, "training_data.pkl")
+                joblib.dump(df_training_labels, "training_labels.pkl")
+
+                lin_reg = joblib.load("./linear_regression_model.pkl")
+            except ValueError as e:
+                return jsonify("Error when retraining - {}".format(e))
+
+            return jsonify("Retrained model successfully.")
+
+
+    @app.route("/currentDetails", methods=['GET'])
+    def current_details():
+        if request.method == 'GET':
+            try:
+                lr = joblib.load("./linear_regression_model.pkl")
+                training_set = joblib.load("./training_data.pkl")
+                labels = joblib.load("./training_labels.pkl")
+
+                return jsonify({"score": lr.score(training_set, labels),
+                                "coefficients": lr.coef_.tolist(), "intercepts": lr.intercept_})
+            except (ValueError, TypeError) as e:
+                return jsonify("Error when getting details - {}".format(e))
+                
     return app
